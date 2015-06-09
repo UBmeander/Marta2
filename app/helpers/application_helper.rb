ae8f8a03a634dd535924c2bd2e3dedd79e5f5a88
Joshua C. Eubanks <ubmeander@gmail.com>
2015-06-09 12:44:13 -0400

    initial

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,20 @@
+module ApplicationHelper
+	def fetch_url_data(source)
+		http = Net::HTTP.get_response(URI.parse(source))
+		data = http.body
+		response = JSON.parse(data)
+		return response		
+	end
+
+	def nearby(lng1, lat1, lng2, lat2)
+		#this method will compare the lat/lng of the User and each bus to see if they're close to each other
+		if (lng1 - lng2).abs <= 0.01 && (lat1 - lat2).abs <= 0.01
+			return true
+		else
+			return false
+			
+		end
+		
+	end
+
+end
